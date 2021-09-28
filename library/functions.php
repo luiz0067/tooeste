d3ddaab37ddf49a95afccda88e7e7ed212c44c4b
Luiz Fernando Brogliatto Ferreira <luiz0067@gmail.com>
2021-09-28 19:49:34 -0300

    Add files via upload

---
+++ library/functions.php
@@ -0,0 +1,21 @@
+<?php
+//	error_reporting(E_ALL);
+//	ini_set('display_errors', '1');
+
+//	error_reporting(E_ALL);
+//	ini_set('display_errors', '1');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require_once($_SERVER['DOCUMENT_ROOT'].'/conect.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/library/functionsDataBase.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/library/functionsFileMangager.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/library/functionsSession.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/library/functionsForms.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/library/functionsEmail.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/library/functionsInteralSSH.php');
+
+	
+
+	
+?>
